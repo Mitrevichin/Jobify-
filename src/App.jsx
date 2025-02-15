ae03085e8ed79cec0a73545e6cb1a1c46016485b
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-02-15 17:11:16 +0200

    react router basic set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomeLayout from './pages/HomeLayout';
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashBoardLayout,
+  Error,
+  Landing,
+} from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
-  },
-  {
-    path: 'about',
-    element: (
-      <div>
-        <h1>About page</h1>
-      </div>
-    ),
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashBoardLayout />,
+      },
+    ],
   },
 ]);
 
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,15 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 function Error() {
-  return <h1>Error Page</h1>;
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to='/'>Back Home</Link>
+    </div>
+  );
 }
 
 export default Error;
